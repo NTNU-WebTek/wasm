bb1a7d36dd6c63ded1bdfac960699c0b14ed17ce
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2023-10-04 18:50:26 +0200

    document code

--- TicTacToeBlazor/Helpers/TicTacToeGame/Cell.cs
+++ TicTacToeBlazor/Helpers/TicTacToeGame/Cell.cs
@@ -1,22 +1,49 @@
 namespace TicTacToeBlazor.Helpers.TicTacToeGame
 {
+    /// <summary>
+    /// Possible states of a cell in a tic-tac-toe game.
+    /// </summary>
     public enum CellState
     {
         None = ' ',
         X = 'X',
         O = 'O'
     }
+    /// <summary>
+    /// Represents a cell in a tic-tac-toe game.
+    /// </summary>
     public class TicTacToeCell
     {
+        /// <summary>
+        /// Gets or sets a value indicating whether this cell is part of a winning line.
+        /// </summary>
         public bool IsWinningCell { get; set; }
 
+        /// <summary>
+        /// Gets the current state of the cell.
+        /// </summary>
         public CellState CurrentState { get; private set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TicTacToeCell"/> class.
+        /// </summary>
+        /// <remarks>
+        /// The cell is initialized with the <see cref="CellState.None"/> state.
+        /// </remarks>
         public TicTacToeCell()
         {
             this.CurrentState = CellState.None;
         }
 
+        /// <summary>
+        /// Plays the cell with the specified state.
+        /// </summary>
+        /// <param name="newState">The new state of the cell. Must be different from <see cref="CellState.None"/>.</param>
+        /// <returns>True if the cell was successfully played, false otherwise.</returns>
+        /// <remarks>
+        /// If the cell is already played, the method does nothing and returns false.
+        /// If parameter <paramref name="newState"/> is <see cref="CellState.None"/>, the method does nothing and returns false.
+        /// </remarks>
         public bool PlayCell(CellState newState)
         {
             bool success = false;
--- TicTacToeBlazor/Helpers/TicTacToeGame/TicTacToeGame.cs
+++ TicTacToeBlazor/Helpers/TicTacToeGame/TicTacToeGame.cs
@@ -1,5 +1,9 @@
 namespace TicTacToeBlazor.Helpers.TicTacToeGame
 {
+
+    /// <summary>
+    ///  The state of tic-tac-toe game.
+    /// </summary>
     public enum GameState
     {
         NotFinished,
@@ -7,12 +11,25 @@ namespace TicTacToeBlazor.Helpers.TicTacToeGame
         WinnerX,
         WinnerO,
     }
+    /// <summary>
+    /// Represents a tic-tac-toe game.
+    /// </summary>
+    /// <remarks>
+    /// The game is played on a 3 by 3 grid of cells, stored in a one-dimensional array.
+    /// (First three cells are the first row, etc.)
+    /// </remarks>
     public class TicTacToeGame
     {
-        public GameState State { get; set; }
+        /// <summary>
+        /// Gets the current state of the game.
+        /// </summary>
+        public GameState State { get; private set; }
         private bool isXNext;
         private readonly TicTacToeCell[] board;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TicTacToeGame"/> class.
+        /// </summary>
         public TicTacToeGame()
         {
             this.State = GameState.NotFinished;
@@ -29,11 +46,30 @@ namespace TicTacToeBlazor.Helpers.TicTacToeGame
             }
         }
 
+        /// <summary>
+        /// Gets the cell at the specified index.
+        /// </summary>
+        /// <remarks>
+        /// The cells are stored in a one-dimensional array.
+        /// (First three cells are the first row, etc.)
+        /// Valid indexes are 0 through 8.
+        /// </remarks>
+        /// <param name="index">index of the cell to return</param>
+        /// <returns>cell at given index</returns>
         public TicTacToeCell GetCell(int index)
         {
             return this.board[index];
         }
 
+        /// <summary>
+        /// Plays the cell at the specified index.
+        /// </summary>
+        /// <remarks>
+        /// The cells are stored in a one-dimensional array.
+        /// (First three cells are the first row, etc.)
+        /// Valid indexes are 0 through 8.
+        /// </remarks>
+        /// <param name="index">index of the cell to play</param>
         public void PlayCell(int index)
         {
             if (this.State == GameState.NotFinished)
@@ -48,6 +84,10 @@ namespace TicTacToeBlazor.Helpers.TicTacToeGame
             }
         }
 
+
+        /// <summary>
+        /// Resets the game to its initial state, i.e. all cells are empty, X starts and state is NotFinished.
+        /// </summary>
         public void ResetGame()
         {
             this.State = GameState.NotFinished;
@@ -55,6 +95,14 @@ namespace TicTacToeBlazor.Helpers.TicTacToeGame
             this.FillBoard();
         }
 
+        /// <summary>
+        /// Returns the <see cref="CellState"/> corresponding to the next move (X or O).
+        /// If the game is finished, returns CellState.None.
+        /// </summary>
+        /// <remarks>
+        /// This is useful for displaying the next move to the user.
+        /// </remarks>
+        /// <returns></returns>
         public CellState NextMove()
         {
             CellState next = CellState.None;
@@ -65,6 +113,9 @@ namespace TicTacToeBlazor.Helpers.TicTacToeGame
             return next;
         }
 
+        /// <summary>
+        /// Checks if the game is finished and updates the State property accordingly.
+        /// </summary>
         private void CheckForWinner()
         {
             TicTacToeCell[] winnerCells = CheckColumns();
@@ -78,8 +129,10 @@ namespace TicTacToeBlazor.Helpers.TicTacToeGame
                 winnerCells = this.CheckDiagnals();
             }
 
+            // There is a winner
             if (winnerCells.Length > 0)
             {
+                // Mark the winning cells
                 foreach (TicTacToeCell cell in winnerCells)
                 {
                     cell.IsWinningCell = true;
@@ -112,13 +165,11 @@ namespace TicTacToeBlazor.Helpers.TicTacToeGame
                 TicTacToeCell ThirdCell = this.board[i + 6];
                 if (IsWInningTriple(firstCell, secondCell, ThirdCell))
                 {
-
                     return new TicTacToeCell[] { firstCell, secondCell, ThirdCell };
                 }
             }
             return Array.Empty<TicTacToeCell>();
         }
-
 
         private TicTacToeCell[] CheckRows()
         {
@@ -152,6 +203,9 @@ namespace TicTacToeBlazor.Helpers.TicTacToeGame
             return Array.Empty<TicTacToeCell>();
         }
 
+        /// <summary>
+        /// Checks if the three cells are all X or all O.
+        /// </summary>
         private static bool IsWInningTriple(TicTacToeCell first, TicTacToeCell second, TicTacToeCell third)
         {
             return first.CurrentState != CellState.None
@@ -159,6 +213,10 @@ namespace TicTacToeBlazor.Helpers.TicTacToeGame
             && second.CurrentState == third.CurrentState;
         }
 
+        /// <summary>
+        /// Checks if the board is full.
+        /// </summary>
+        /// <returns>true if the board is full, false otherwise</returns>
         private bool IsFull()
         {
             int i = 0;
